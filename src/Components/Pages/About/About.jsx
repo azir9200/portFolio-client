0e65685badf2c41560630e4b8d48f01bfd6b80c0
Azir@9200 <aziruddin83@hotmail.com>
2024-02-13 01:19:30 +0100

    added

--- src/Components/Pages/About/About.jsx
+++ src/Components/Pages/About/About.jsx
@@ -1,10 +1,25 @@
+import { Link } from "react-router-dom";
 
 
 const About = () => {
   return (
-    <div className="mx-auto text-center   sm:m-4 md:m-12 sm:p-4 md:p-12 bg-slate-300  border">
+
+    <div className="hero min-h-screen bg-base-200 border border-sky-200">
+    <div className="hero-content flex-col lg:flex-row-reverse">
+     
+      <div className="bg-lime-100 max-w-xl  p-8 border border-b-slate-300">
       <h2>I am passionate about what I do and possess a steady source of motivation that drives me to always do my best. Recognized for excellence in cruise service and adeptness in optimizing restaurant operations to ensure exceptional guest experiences. By combining my managerial expertise with newfound tech skills, I am determined to introduce innovative solutions that enhance both service quality and operational efficiency in the hospitality sector.</h2>
+        
+      </div>
+      <div className=" "> 
+     <h2 className="text-4xl w-1/3">About  Myself </h2>
+      </div>
     </div>
+  </div>
+
+
+
+   
   );
 };
 
--- src/Components/Pages/Cover/Cover.jsx
+++ src/Components/Pages/Cover/Cover.jsx
@@ -3,20 +3,22 @@ import { Link } from "react-router-dom";
 
 const Cover = () => {
   return (
-    <div>
-      <div className="hero min-h-screen bg-base-200">
+   
+  <div className="hero min-h-screen bg-base-200 border border-sky-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src="https://i.ibb.co/9Vx9Hvt/azir.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-    <div>
+    <div className=" "> 
+    <img src="https://i.ibb.co/9Vx9Hvt/azir.jpg" className="max-w-xs rounded-lg shadow-2xl" />
+    </div>
+    <div className="bg-lime-100 max-w-xl  p-8 border border-b-slate-300">
       <h2 className="text-5xl font-bold">Hi there, I am </h2>
       <h1 className="text-5xl font-bold">AZIR UDDIN</h1>
-      <p className="py-6">I ama a Junior Frontend Web Developer based in Zurich, Switzerland.</p>
+      <p className="py-6">I am a Junior Frontend Web Developer based in Zurich, Switzerland.</p>
       <Link to='/contacts'>  
       <button className="btn btn-primary">Contact Me</button> </Link>
     </div>
   </div>
 </div>
-    </div>
+    
   );
 };
 
